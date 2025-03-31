0a93cdc9ee28dac8da310579b62fbe372191fb2c
kang-san <junbuck01@naver.com>
2025-03-31 19:35:20 +0900

    update

--- src/shared/utils/crypto.ts
+++ src/shared/utils/crypto.ts
@@ -1,4 +1,4 @@
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import crypto from 'crypto';
 
 const AES_KEY = process.env['AES_KEY'] ?? 'abcde12345abcde12345abcde12345ab';
@@ -6,10 +6,16 @@ const IV = AES_KEY.substring(0, 16);
 
 export const generateHash = (data: string | Buffer) => {
   const salt = bcrypt.genSaltSync(Number(process.env['SALT_ROUNDS']));
-  return bcrypt.hashSync(data, salt);
+  // Buffer를 문자열로 변환
+  const dataString = Buffer.isBuffer(data) ? data.toString() : data;
+  return bcrypt.hashSync(dataString, salt);
 };
 
-export const compareHash = (target: string | Buffer, hash: string) => bcrypt.compareSync(target, hash);
+export const compareHash = (target: string | Buffer, hash: string) => {
+  // Buffer를 문자열로 변환
+  const targetString = Buffer.isBuffer(target) ? target.toString() : target;
+  return bcrypt.compareSync(targetString, hash);
+};
 
 export const encryptAES = (text: string) => {
   const cipher = crypto.createCipheriv('AES-256-GCM', Buffer.from(AES_KEY), IV);
